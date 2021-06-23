2951b5f52e0fbe4ad586a516a0e76e7b09e224ea
Jamal RABHIOUAN <jamrabhi@e2r9p6.42.fr>
2021-06-24 00:31:23 +0200

    F et C RGB OK

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: jamrabhi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 17:33:00 by jamrabhi          #+#    #+#             */
-/*   Updated: 2021/06/18 21:54:53 by jamrabhi         ###   ########.fr       */
+/*   Updated: 2021/06/24 00:29:30 by jamrabhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,28 +87,77 @@ void	get_EA(const char *line, t_map *map)
 	}
 }
 
+
+int		check_number_elements(char **array)
+{
+	int i;
+
+	i=0;
+	while(array[i])
+		i++;
+	return i;
+}
+void	show_array(char **array)
+{
+	int i;
+
+	i=0;
+	while(array[i])
+	{
+		printf("Line[%d] = |%s|\n", i, array[i]);
+		i++;
+	}
+}
+
+
 void	get_F_RGB(const char *line, t_map *map)
 {
 	char	**numbers;
-//	int		RGB[3];
+	int		RGB[3];
+	int size;
 
 	numbers = ft_split(line, ' ');
 	if (line && numbers[0] && (ft_strncmp(numbers[0], "F", 1) == 0))
 	{
 		numbers = ft_split(numbers[1], ',');
-		if (numbers[2] && !numbers[3] && ft_isdigit_str(numbers[0]) &&
-				ft_isdigit_str(numbers[1]) && ft_isdigit_str(numbers[2]))
+		size = check_number_elements(numbers);
+		if (!(size == 3 && ft_isdigit_str(numbers[0]) && ft_isdigit_str(numbers[1]) && ft_isdigit_str(numbers[2])))
+			print_error("Fichier .cub incorrect");
+		RGB[0] = ft_atoi(numbers[0]);
+		RGB[1] = ft_atoi(numbers[1]);
+		RGB[2] = ft_atoi(numbers[2]);
+		if (!((RGB[0] >= 0 && RGB[0] <= 255) && (RGB[1] >= 0 && RGB[1] <= 255) && (RGB[2] >= 0 && RGB[2] <= 255)))
 			print_error("Fich ier .cub incorrect");
-	//	RGB[0] = ft_atoi(numbers[0]);
-	//	RGB[1] = ft_atoi(numbers[1]);
-	//	RGB[2] = ft_atoi(numbers[2]);
-	//	printf("%d\n", RGB[0]);
-	//	printf("%d\n", RGB[1]);
-	//	printf("%d\n", RGB[2]);
+		map->F[0] = RGB[0];
+		map->F[1] = RGB[1];
+		map->F[2] = RGB[2];
 	}
-
-		map->C[1] = 2;
 }
+
+void	get_C_RGB(const char *line, t_map *map)
+{
+	char	**numbers;
+	int		RGB[3];
+	int size;
+
+	numbers = ft_split(line, ' ');
+	if (line && numbers[0] && (ft_strncmp(numbers[0], "C", 1) == 0))
+	{
+		numbers = ft_split(numbers[1], ',');
+		size = check_number_elements(numbers);
+		if (!(size == 3 && ft_isdigit_str(numbers[0]) && ft_isdigit_str(numbers[1]) && ft_isdigit_str(numbers[2])))
+			print_error("Fich ier .cub incorrect");
+		RGB[0] = ft_atoi(numbers[0]);
+		RGB[1] = ft_atoi(numbers[1]);
+		RGB[2] = ft_atoi(numbers[2]);
+		if (!((RGB[0] >= 0 && RGB[0] <= 255) && (RGB[1] >= 0 && RGB[1] <= 255) && (RGB[2] >= 0 && RGB[2] <= 255)))
+			print_error("Fich ier .cub incorrect");
+		map->C[0] = RGB[0];
+		map->C[1] = RGB[1];
+		map->C[2] = RGB[2];
+	}
+}
+
 
 void	parse_line(char *file_cub, t_map *map)
 {
@@ -123,5 +172,6 @@ void	parse_line(char *file_cub, t_map *map)
 		get_WE(line, map);
 		get_EA(line, map);
 		get_F_RGB(line, map);
+		get_C_RGB(line, map);
 	}
 }
