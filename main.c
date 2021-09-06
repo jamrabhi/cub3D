1b7857944d3506e7cadec794ae3fc6d1c7bae939
Jamal RABHIOUAN <jamrabhi@e2r3p1.42.fr>
2021-09-06 22:41:47 +0200

    norm fix

--- main.c
+++ main.c
@@ -11,27 +11,22 @@
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
 #include <fcntl.h>
 
 int	main(int argc, char **argv)
 {
-	t_map map;
+	t_map	map;
 
 	ft_bzero(&map, sizeof(map));
 	if (argc > 1)
 		parse_line(argv[1], &map);
-
-	
 	printf("-------------------------\n_________________________\n");
 	printf("INFO DATA STRUCT. :\n");
-	printf("NO : %s\n", map.NO_path);
-	printf("SO : %s\n", map.SO_path);
-	printf("WE : %s\n", map.WE_path);
-	printf("EA : %s\n", map.EA_path);
-	printf("FLOOR : R = %d, G= %d, B = %d\n", map.F[0], map.F[1], map.F[2]);
-	printf("CEILING : R = %d, G= %d, B = %d\n", map.C[0], map.C[1], map.C[2]);
+	printf("NO : %s\n", map.no_path);
+	printf("SO : %s\n", map.so_path);
+	printf("WE : %s\n", map.we_path);
+	printf("EA : %s\n", map.ea_path);
+	printf("FLOOR : R = %d, G= %d, B = %d\n", map.f[0], map.f[1], map.f[2]);
+	printf("CEILING : R = %d, G= %d, B = %d\n", map.c[0], map.c[1], map.c[2]);
 	printf("-------------------------\n_________________________\n");
-
-
 }
