f41989dd7cc2ed6f243223cccde9cb1d9016cc97
Jamal RABHIOUAN <jamrabhi@e2r7p3.42.fr>
2021-09-19 23:21:51 +0200

    parse 0 N S E W in map

--- parse_map.c
+++ parse_map.c
@@ -112,6 +112,72 @@ int	check_last_line(char **map)
 	return (1);
 }
 
+int	check_borders(char **map)
+{
+	int i;
+	int j;
+	int last_char;
+
+	i = 0;
+	j = 0;
+	last_char = (ft_strlen(map[i]) - 1);
+	while (map[i])
+	{
+		j = 0;
+		last_char = (ft_strlen(map[i]) - 1);
+		if (map[i][j] == ' ' || map[i][last_char] == ' ')
+		{
+			while (map[i][j] && map[i][j] == ' ')
+				j++;
+			while (map[i][last_char] > 0 && map[i][last_char] == ' ')
+				last_char--;
+		}
+		//printf("DEBUG line = '%d' first = '%c' last = '%c'\n", i, map[i][j], map[i][last_char]);
+		if (map[i][j] != '1' || map[i][last_char] != '1')
+		{
+			//printf("map[%d] = '%s'\n",i, map[i]);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	empty_space(char c)
+{
+	if (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		return (1);
+	return (0);
+}
+
+int	check_map(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (map[i])
+	{
+		while (map[i][j])
+		{
+			if ((map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S' || 
+					map[i][j] == 'E' || map[i][j] == 'W') && (!(empty_space(map
+					[i][j + 1]) && empty_space(map[i - 1][j]) &&
+					empty_space(map[i + 1][j]))))
+			{
+				//printf("MAP i+1 = '%s'\n", map[i+1 ]);
+				printf("line = '%d' - pos = '%d' - j = '%c' - j+1 = '%c' - i-1 = '%c'  i+1 = '%c'\n",i, j, map[i][j], map[i][j+1], map[i-1][j], map[i+1][j]);
+				return 0;
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (1);
+}
+
 void	parse_map(char *str, t_map *map)
 {
 	int	i;
@@ -126,10 +192,12 @@ void	parse_map(char *str, t_map *map)
 		ft_isprint('0');
 	if (check_valid_char(str) == 0)
 		print_error("Invalid map");
-	//printf("max = %d", check_max_length(str));
 	if (!(check_first_line(map_array) && check_last_line(map_array)))
 		print_error("Invalid map");
-
+	if (!check_borders(map_array))
+		print_error("Invalid map (borders)");
+	if (!check_map(map_array))
+		print_error("Invalid map (0)");
 	/*
 	while (map_array[i])
 	{
